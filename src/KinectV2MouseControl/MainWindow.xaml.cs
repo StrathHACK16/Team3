3f7c35cac939e9081ff41369a9cbfb9435783484
Colin Cheung <colincheung25@gmail.com>
2016-02-21 05:00:00 +0000

    Removed GUI components

--- src/KinectV2MouseControl/MainWindow.xaml.cs
+++ src/KinectV2MouseControl/MainWindow.xaml.cs
@@ -11,42 +11,6 @@ namespace KinectV2MouseControl
         {
             InitializeComponent();
         }
-        
-        private void MouseSensitivity_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (MouseSensitivity.IsLoaded)
-            {
-                kinectCtrl.mouseSensitivity = (float)MouseSensitivity.Value;
-                txtMouseSensitivity.Text = kinectCtrl.mouseSensitivity.ToString("f2");
-                
-                Properties.Settings.Default.MouseSensitivity = kinectCtrl.mouseSensitivity;
-                Properties.Settings.Default.Save();
-            }
-        }
-
-        private void txtMouseSensitivity_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                float v;
-                if (float.TryParse(txtMouseSensitivity.Text, out v))
-                {
-                    MouseSensitivity.Value = v;
-                    kinectCtrl.mouseSensitivity = (float)MouseSensitivity.Value;
-                }
-            }
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            MouseSensitivity.Value = Properties.Settings.Default.MouseSensitivity;
-
-        }
-
-        private void btnDefault_Click(object sender, RoutedEventArgs e)
-        {
-            MouseSensitivity.Value = KinectControl.MOUSE_SENSITIVITY;
-        }
 
         private void chkNoClick_Checked(object sender, RoutedEventArgs e)
         {
